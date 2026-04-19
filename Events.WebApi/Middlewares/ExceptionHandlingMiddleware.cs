01bb8fb6d6f5107be0a242e743d24083043ecdad
ilya.efremov <ilya.efremov@fstravel.com>
2026-04-19 18:22:37 +0300

    Добавлено логирование ошибок

--- Events.WebApi/Middlewares/ExceptionHandlingMiddleware.cs
+++ Events.WebApi/Middlewares/ExceptionHandlingMiddleware.cs
@@ -6,11 +6,14 @@ namespace Events.WebApi.Middlewares
     public class ExceptionHandlingMiddleware
     {
         private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger;
 
         public ExceptionHandlingMiddleware(
-            RequestDelegate next)
+            RequestDelegate next,
+            ILogger<ExceptionHandlingMiddleware> logger)
         {
             _next = next;
+            _logger = logger;
         }
 
         public async Task Invoke(
@@ -22,6 +25,10 @@ namespace Events.WebApi.Middlewares
             }
             catch (Exception ex)
             {
+                _logger.LogError(ex, "Error while execute: {Method} {Path}",
+                    context.Request.Method,
+                    context.Request.Path);
+
                 await HandleExceptionAsync(context, ex);
             }
         }
